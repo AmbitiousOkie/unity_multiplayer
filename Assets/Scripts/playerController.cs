762ab85b4af592828f3c1bc6b7571b2d2fbe4baa
Kris <kris@urbanscouter.com>
2016-09-10 16:18:25 -0500

    Added playerControler.cs

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -1,15 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class playerController : MonoBehaviour {
+namespace S3
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+{
+
+	public class playerController : MonoBehaviour {
+
+		// Use this for initialization
+		void Start () {
+
+
+		
+		}
+		
+		// Update is called once per frame
+		void Update () {
+
+			float x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+			float y = Input.GetAxis ("Vertical") * Time.deltaTime * 3.0f;
+
+			transform.Rotate (0, x, 0);
+			transform.Translate (0, 0, y);
+		
+		}
 	}
 }
