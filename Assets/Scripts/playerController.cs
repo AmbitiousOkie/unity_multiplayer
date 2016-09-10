990681f1553ce127f94120c76e78385ec0f0a1fc
Kris <kris@urbanscouter.com>
2016-09-10 16:26:26 -0500

    added networking awareness to playerController

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -1,11 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.Networking;
 
 namespace S3
 
 {
 
-	public class playerController : MonoBehaviour {
+	public class playerController : NetworkBehaviour {
 
 		// Use this for initialization
 		void Start () {
@@ -16,6 +17,10 @@ namespace S3
 		
 		// Update is called once per frame
 		void Update () {
+
+			if (!isLocalPlayer) {
+				return;
+				}
 
 			float x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
 			float y = Input.GetAxis ("Vertical") * Time.deltaTime * 3.0f;
